d682c5df5f478aed9dd5ec1b06ef4362387403e7
irfanjamil <irfanjamil9@gmail.com>
2023-03-30 13:29:06 -0400

    scripts that can scrape specific data from nba.com website and store data in csv files

---
+++ scraper_general.py
@@ -0,0 +1,66 @@
+from bs4 import BeautifulSoup as bs4
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import csv
+
+urls = ['https://www.nba.com/stats/teams/traditional','https://www.nba.com/stats/teams/advanced','https://www.nba.com/stats/teams/opponent','https://www.nba.com/stats/teams/defense']
+driver = webdriver.Chrome('/Users/irfanjamil/chromedriver/chromedriver')
+
+for st in range(2000,2022):
+    end = st+1-2000
+    if end < 10:
+        end = '0'+str(end)
+    else:
+        end = str(end)
+    season_str = '?Season='+str(st)+'-'+end
+    for url in urls:
+        print(st, url, sep = ', ')
+        driver.get(url+season_str)
+        html = driver.page_source
+        soup = bs4(html,"html.parser")
+        #print(soup.prettify())
+        tables = soup.find_all("table", class_="Crom_table__p1iZz")
+        table = tables[0]
+        thead = table.thead
+        cols = thead.find_all('th')
+        titles = []
+        header_idx = 0
+        for col in cols:
+            if header_idx == 0:
+                header_idx += 1
+                continue
+            if col.has_attr('hidden'):
+                break
+            title = col.contents[0]
+            titles.append(title)
+        list_of_rows = []
+        body = table.tbody
+        rows = body.find_all('tr')
+        for row in rows:
+            idx = 0
+            tds = row.find_all('td')
+            values = []
+            for td in tds:
+                if idx == 0:
+                    idx+=1
+                    continue
+                elif idx == 1:
+                    try:
+                        span = td.span
+                        values.append(span.contents[0])
+                        idx+=1
+                    except:
+                        values.append(td.a.string)
+                        idx+=1
+                    continue    
+                else:
+                    s_val = td.string.replace(',','')
+                    values.append(float(s_val))
+            list_of_rows.append(values[:])
+        with open(season_str[season_str.find('=')+1:]+'_general_' + url[url.rfind('/')+1:] + '.csv','w') as f:
+            writer = csv.writer(f)
+            writer.writerow(titles[:])
+            writer.writerows(list_of_rows[:])
+
+
